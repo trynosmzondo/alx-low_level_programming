d511ac6573df50d9951394d953fdc72d41c72ea2
trynosmzondo <trynosm68@gmail.com>
2022-04-07 10:11:57 -0700

    My 4puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+
 /**
- *main - prints the phrase Programming is like bulding a multilingual puzzle
- *
- *Retun: 0 after printing the function
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
- 
-int main(void) 
-{ 
+int main(void)
 
+{   
 
-          puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
+             
+	return (0);
 
-	  
-           return (0);
 }
