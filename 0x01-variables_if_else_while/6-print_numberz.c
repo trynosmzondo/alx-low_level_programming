2816cd698208a340c6e08e0a2dde799d372cc3a2
trynosmzondo <trynosm68@gmail.com>
2022-04-08 16:06:03 -0700

    My 6print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - rints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return(0);
+}
