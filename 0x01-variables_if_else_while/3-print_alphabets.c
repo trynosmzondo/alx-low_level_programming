92918518a1e881ce5cb5a4cadcfd434bf72edd62
trynosmzondo <trynosm68@gmail.com>
2022-04-08 14:15:18 -0700

    My3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,12 @@
  * followed by a new line
  * Return:  0 always 
  */
-
 int main(void)
 {	
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-
-	{
+{
 		putchar(ch);
 	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
