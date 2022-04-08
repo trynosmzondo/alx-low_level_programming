bc0f47c3bc1b4374b4594c54379efc9fe0bbc390
trynosmzondo <trynosm68@gmail.com>
2022-04-08 14:00:46 -0700

    My alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,8 +14,8 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
-		putchar(ch);
+		putchar (ch);
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
