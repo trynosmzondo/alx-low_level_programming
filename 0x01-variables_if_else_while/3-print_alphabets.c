b5075fccfe072ae1e9eb8df0e80b9da2a18dcbfa
trynosmzondo <trynosm68@gmail.com>
2022-04-08 13:52:39 -0700

    My3print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - program that prints the alphabet in lowercase, and then in uppercase
+ * followed by a new line
+ * Return:  0 always 
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
