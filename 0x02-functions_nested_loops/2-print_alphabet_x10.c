67550c79052ce83b37e411420757aededcde5493
trynosmzondo <trynosm68@gmail.com>
2022-04-12 15:15:16 -0700

    My commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+       	int t = 0;
+	char l;
+
+	while (t < 10)
+	{
+		if (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+			l++;
+		}
+		_putchar('\n');
+
+		t++;
+	}
+}
