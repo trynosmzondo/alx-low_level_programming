84439c3baf3a2de906a806d48caba04e9f68c40e
trynosmzondo <trynosm68@gmail.com>
2022-05-03 06:39:35 -0700

    My commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-/**
- * main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", argv[0]);
-	return (0);
-}
+ #include <stdio.h>
+  2 /**
+  3  * main - prints its name, followed by a new line.
+  4  * @argc: number of command line arguments.
+  5  * @argv: array that contains the program command line arguments.
+  6  * Return: 0 - success.
+  7  */
+  8 int main(int argc __attribute__((unused)), char *argv[])
+  9 {
+ 10         printf("%s\n", argv[0]);
+ 11         return (0);
+ 12 }
+
+
