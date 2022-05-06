1c2231818fcfa87ba16b9cb9fcfabbfb89e808f5
trynosmzondo <trynosm68@gmail.com>
2022-05-05 23:47:30 -0700

    My commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: width of the array.
- * @height: height of the array.
- *
- * Return: pointer of an array of integers
+ * alloc_grid - creates a 2 dimensional array of integers initialized to 0
+ * @width: width of array
+ * @height: height of array
+ * Return: a double pointer to the 2D array
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **gridout;
 	int i, j;
+	int **a;
 
-	if (width < 1 || height < 1)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	gridout = malloc(height * sizeof(int *));
-	if (gridout == NULL)
-	{
-		free(gridout);
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
 		return (NULL);
-	}
-
 	for (i = 0; i < height; i++)
 	{
-		gridout[i] = malloc(width * sizeof(int));
-		if (gridout[i] == NULL)
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)					
 		{
-			for (i--; i >= 0; i--)
-				free(gridout[i]);
-			free(gridout);
+			for (j = 0; j < i; j++)
+				free(a[j]);
+			free(a);
 			return (NULL);
 		}
-	}
-
-	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
-			gridout[i][j] = 0;
-
-	return (gridout);
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
 }
