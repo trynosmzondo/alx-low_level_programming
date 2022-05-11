9af60c7d3d1405d82a9061ca4ca33b0ad1b0595e
trynosmzondo <trynosm68@gmail.com>
2022-05-10 19:06:27 -0700

    My commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,3 +60,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	new_name = _copy(name);
+	if (new_name == NULL)
+	{
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).name = new_name;
+
+	(*snoopie).age = age;
+
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*snoopie).name);
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).owner = new_owner;
+
+	return (snoopie);
+}
